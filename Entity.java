8817062e0830cd09aee227d890a9ded8b6c46d01
Abel Berhanu <abelberhanu005@gmail.com>
2024-12-18 14:12:16 -0500

    Create Entity.java

---
+++ Entity.java
@@ -0,0 +1,17 @@
+public class Entity {
+    private final String name;
+    private final String description;
+
+    public Entity(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
